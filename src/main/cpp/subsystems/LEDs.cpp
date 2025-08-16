513f14f31cb363de54e91c1f36e59114592746cb
Lander90WasTaken <ultralander90@gmail.com>
2025-08-15 21:09:34 -0500

    Added experimental 12v RGB Functions and subsystems

--- src/main/cpp/subsystems/LEDs.cpp
+++ src/main/cpp/subsystems/LEDs.cpp
@@ -1,8 +1,8 @@
 #include <subsystems/LEDS.h>
 
 LEDS::LEDS(){
-        m_led.Set(0);
-        areTheyOnBro = false; // Turn off the LED initially
+        ..m_led.Set(0);
+        //areTheyOnBro = false; // Turn off the LED initially
 }
 
 void LEDS::TeleopPeriodic(){
