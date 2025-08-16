61792c00166c208cfa0b18876bf13c93804ab553
Lander90WasTaken <ultralander90@gmail.com>
2025-08-15 21:09:21 -0500

    Foefwe

--- src/main/cpp/subsystems/LEDs.cpp
+++ src/main/cpp/subsystems/LEDs.cpp
@@ -10,13 +10,21 @@ void LEDS::TeleopPeriodic(){
 }
 
 void LEDS::SetLEDColor(int redValue, int greenValue, int blueValue) {
-        
+        if(redValue <= 255 && greenValue <= 255 && blueValue <= 255){
+                m_ledRed.SetPulseTime(255-redValue); //Inverts the color so the MOSFETs can allow the PWM signal to the lights.
+                m_ledGreen.SetPulseTime(255-greenValue); //SetPulseTime may not be correct function and will need to be tested and likely changed.
+                m_ledBlue.SetPulseTime(255-blueValue);
+        } else{
+                m_ledRed.SetPulseTime(255);
+                m_ledGreen.SetPulseTime(0);
+                m_ledBlue.SetPulseTime(0);
+        }
 }
-void LEDS::TurnOnLED(bool value){
+/*void LEDS::TurnOnLED(bool value){
         m_led.Set(value);
         areTheyOnBro = true; // Turn on the LED
 }
 
 bool LEDS::areTheyOn(){
-    return areTheyOnBro;
-}
+        return areTheyOnBro;
+}*/
--- src/main/include/subsystems/LEDS.h
+++ src/main/include/subsystems/LEDS.h
@@ -9,13 +9,15 @@ class LEDS : public frc2::SubsystemBase {
 
     void TeleopPeriodic();
 
-    void TurnOnLED(bool value);
-    void TurnOffLED();
-    void SetLEDColor();
-    bool areTheyOn();
+    //void SetLED(bool value);
+    void setLEDColor(int redValue, int greenValue, int blueValue);
+    //void TurnOffLED();
+    //bool areTheyOn();
 
 
 private:
-    frc::DigitalOutput m_led{3}; // PWM pin 1
+    frc::DigitalOutput m_ledRed{3}; // PWM pin 1
+    frc::DigitalOutput m_ledGreen{4}; // PWM pin 1
+    frc::DigitalOutput m_ledBlue{5}; // PWM pin 1
     bool areTheyOnBro;
 };
